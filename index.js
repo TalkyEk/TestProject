c821cd91a1b2d86fbfa6df6fa16d757dfccc9ebc
Yehor <koniukhov.yehor@gmail.com>
2019-04-16 23:06:31 +0300

    fix: change function, feat: make a livechange function

--- index.js
+++ index.js
@@ -1,53 +1,32 @@
-let add = document.getElementById('tovlist');
-let tik = document.getElementById('post');
+const add = document.getElementById('tovlist');
+const tik = document.getElementById('post');
+let test;
+const index = Date.now();
 
 tik.addEventListener('click', function() {
   const url = document.getElementById('inputurl').value;
   const name = document.getElementById('inputname').value;
   const desc = document.getElementById('inputdesc').value;
   
-  let tovTemplate = `<div>\
-				<img src="${url}" alt="${name}" class="img">\
-				<h2 class="name-product id="${name}">${name}</h2>\
-				<p class="description">${desc}</p>\
-				<button id="button-del">Delete</button>\
-				<button id="button-cg">Change</button>\
-				</div>`;
+  let tovTemplate = createpost(url, name, desc, index);
   add.innerHTML += tovTemplate;
   if(localStorage.getItem('microbd') != undefined) {
     const dobavka = JSON.parse(localStorage.getItem('microbd'));
-    dobavka.push({ tovar: url, name: name, description: desc});
+    dobavka.push({ url, name, description: desc, index});
+    createdelevent();
     return localStorage.setItem('microbd', JSON.stringify(dobavka));
   }
-  tovdata.push({ tovar: url, name: name, description: desc });
+  tovdata.push({ url, name, description: desc, index});
   localStorage.setItem('microbd', JSON.stringify(tovdata));
+  createdelevent();
 });
 
 window.onload = function() {
   if(localStorage.getItem('microbd') !== null) {
-    const posting = JSON.parse(localStorage.getItem('microbd'));
-    
-    posting.forEach(function(e, index) {
-      if(e === 'delete') return;
-      let tovTemplate = `<div>\
-				<img src="${e.tovar}" alt="${e.name}" class="img">\
-				<h2 class="name-product" id="${e.name}">${e.name}</h2>\
-				<p class="description">${e.description}</p>\
-				<button id="button-del" data-index=${index}>Delete</button>\
-				<button id="button-cg" class="button-cg">Change</button>\
-			</div>`;
+    const posting = JSON.parse(localStorage.getItem('microbd')); 
+    posting.forEach(function(e) {
+      let tovTemplate = createpost(e.url, e.name, e.description, e.index);
       add.innerHTML += tovTemplate;
-    });
-  }
-  
-  const btn = document.querySelectorAll('#button-del');
-  for(let i = 0; i < btn.length; i++) {
-    btn[i].addEventListener('click', function() {
-      let data1 = this.dataset.index;
-      btn[i].parentElement.innerHTML = '';
-      let xz = JSON.parse(localStorage.getItem('microbd'));
-      xz.splice(data1, 1, 'delete');
-      localStorage.setItem('microbd', JSON.stringify(xz));
     });
   }
   
@@ -57,11 +36,10 @@ window.onload = function() {
   
   btnChange.forEach(function (btn) {
     btn.onclick = function() {
-      modal.style.display = 'block';
+      modal.style.display = 'block'; 
       const currentmodal = btn.parentElement;
-      const indexmodal = currentmodal.querySelector("#button-del").dataset.index;
-      const currentedit = document.querySelector("#myModal");
-      currentedit.setAttribute("index", indexmodal);
+      test = currentmodal.querySelector("#button-del").dataset.index;
+      console.log(test);
     };
   });
   
@@ -78,16 +56,41 @@ window.onload = function() {
   const changes = document.getElementById('change');
   changes.onclick = function(e) {
   	event.preventDefault();
-  	let data1 = e.currentTarget;
   	const newurl = document.getElementById('inputnewurl').value;
     const newname = document.getElementById('inputnewname').value;
     const newdesc = document.getElementById('inputnewdesc').value;
-    const curedit = data1.parentElement.parentElement.parentElement.parentElement.parentElement.parentElement.getAttribute("index");
-    console.log(curedit);
     let xz = JSON.parse(localStorage.getItem('microbd'));
-    xz.splice(curedit, 1, { tovar: newurl, name: newname, description: newdesc});
+    const curentelement= xz.map((e) => {return e.index;}).indexOf(parseInt(test, 10));
+    xz.splice(curentelement, 1, { url: newurl, name: newname, description: newdesc, index});
+    console.log(xz);
     localStorage.setItem('microbd', JSON.stringify(xz));
     modal.style.display = 'none';
-    location.reload();
+    livechange(newurl, newname, newdesc);
   }
 };
+function createpost (url, name, description, index) {
+	let tovTemplate = `<div>\
+				<img src="${url}" alt="${name}" class="img">\
+				<h2 class="name-product" id="${name}">${name}</h2>\
+				<p class="description">${description}</p>\
+				<button id="button-del" data-index=${index}>Delete</button>\
+				<button id="button-cg" class="button-cg">Change</button>\
+			</div>`;
+	return tovTemplate;
+}
+function livechange (url, name, description) {
+	const oldPost = document.querySelector(`[data-index="${test}"]`).parentElement;
+	oldPost.innerHTML = createpost(url, name, description);
+}
+function createdelevent() {
+	const btn = document.querySelectorAll('#button-del');
+	for(let i = 0; i < btn.length; i++) {
+	    btn[i].addEventListener('click', function() {
+		    let data1 = this.dataset.index;
+		    btn[i].parentElement.innerHTML = '';
+		    let xz = JSON.parse(localStorage.getItem('microbd'));
+		    const newxz = xz.filter((el) => el.index != data1);
+		    localStorage.setItem('microbd', JSON.stringify(newxz));
+	    });
+  	}
+}
